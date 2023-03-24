3dfd9cfa4903c1d98309dedb08134cf81255aa52
Garuizbido <garuizbido@gmail.com>
2023-03-24 16:14:46 -0400

    Added Bluetooth Manager

---
+++ Hand View/Model/BluetoothManager.swift
@@ -0,0 +1,69 @@
+//
+//  BluetoothManager.swift
+//  Hand View
+//
+//  Created by Gabriel A. Ruiz Bido on 3/24/23.
+//
+
+import SwiftUI
+import CoreBluetooth
+
+class BluetoothManager: NSObject, CBCentralManagerDelegate, CBPeripheralDelegate {
+
+    var centralManager: CBCentralManager?
+    var peripheral: CBPeripheral?
+    var characteristic: CBCharacteristic?
+    let serviceUUID = CBUUID(string: "0xFFE0")
+    let characteristicUUID = CBUUID(string: "0xFFE1")
+    
+    override init() {
+        super.init()
+        centralManager = CBCentralManager(delegate: self, queue: nil)
+    }
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if central.state == .poweredOn {
+            print("Bluetoth is scanning")
+            central.scanForPeripherals(withServices: [serviceUUID], options: nil)
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        if peripheral.name == "DSD TECH" {
+                    central.stopScan()
+                    self.peripheral = peripheral
+                    peripheral.delegate = self
+                    central.connect(peripheral, options: nil)
+                }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        peripheral.discoverServices([serviceUUID])
+        print("Device connected")
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        if let services = peripheral.services {
+            for service in services {
+                peripheral.discoverCharacteristics([characteristicUUID], for: service)
+            }
+        }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+        if let characteristics = service.characteristics {
+            for characteristic in characteristics {
+                if characteristic.uuid == characteristicUUID {
+                    self.characteristic = characteristic
+                    peripheral.setNotifyValue(true, for: characteristic)
+                }
+            }
+        }
+    }
+    
+    func writeValue(_ value: Int) {
+        var temp = value
+        let data = Data(bytes: &temp, count: 1)
+        peripheral?.writeValue(data, for: characteristic!, type: .withoutResponse)
+    }
+}
